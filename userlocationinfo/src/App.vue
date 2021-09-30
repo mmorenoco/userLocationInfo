f34b40c0f95664e4dd808b0a1f8548b94143790f
mmorenoco <mmcorrales@outlook.com>
2021-09-30 20:11:26 +0200

    starting point

--- userlocationinfo/src/App.vue
+++ userlocationinfo/src/App.vue
@@ -17,8 +17,12 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family:  'Geomanist webfont';
+  src: url(fonts/Geomanist-Regular.otf);
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Geomanist webfont',Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
