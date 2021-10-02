2d07c6d02012feb1d0d54eea3ccef9fc94444db7
mmorenoco <mmcorrales@outlook.com>
2021-10-02 18:56:58 +0200

    components and store

--- userlocationinfo/src/App.vue
+++ userlocationinfo/src/App.vue
@@ -1,32 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
 
 <style>
-@font-face {
-  font-family:  'Geomanist webfont';
-  src: url(fonts/Geomanist-Regular.otf);
-}
-#app {
-  font-family: 'Geomanist webfont',Arial,sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family:  'Geomanist webfont';
+    src: url(fonts/Geomanist-Regular.otf);
+  }
+ 
+  :root {
+    --main: #1F2326;
+    --secondary: #7C9AA6;
+    --details-one: #2D6673;
+    --details-two: #5C6F73;
+  }
+
+  html {
+    background-color: var(--details-one);
+  }
+
+  body {
+    margin: 0 auto;
+  }
+
+  #app {
+    font-family: 'Geomanist webfont',Arial,sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    /* background-color: var(--details-two); */
+    color: #2c3e50;
+    margin: 0px;
+    margin-top: 100px;
+  }
+
 </style>
---
+++ userlocationinfo/src/components/Map.vue
@@ -0,0 +1,96 @@
+<template>
+<div class="map-container">
+  <div id="map">
+
+  </div>
+</div>
+</template>
+
+<script>
+import { Loader } from "@googlemaps/js-api-loader"
+
+// import { mapState } from 'vuex';
+// import { mapActions } from 'vuex';
+
+export default {
+    name: 'Maps',
+    methods: {
+        // ...mapActions('properties', ['updatePropertiesList']),
+        loadMap() {
+
+            const loader = new Loader({
+                apiKey: "",
+                version: "weekly",
+                libraries: ["places"]
+            })
+
+            const mapOptions = {
+                center: {
+                    lat: 40.2085,
+                    lng: -3.713
+                },
+                zoom: 6
+            }
+
+            loader
+            .load()
+            .then((google) => {
+                const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+                new google.maps.Marker({
+                    position: { lat: -40.416729, lng: -3.703339},
+                    map: map,
+                    title: "Madrid",
+                })
+
+                new google.maps.Marker({
+                    position: { lat: 36.67777, lng: -5.446},
+                    map: map,
+                    title: "Ubrique",
+                })
+
+                return map
+            })
+
+            .catch(e => {
+                console.log(e)
+            })
+        }
+    },
+    async mounted() {
+         await this.loadMap()
+    },
+    // computed: {
+    //     // ...mapState('properties', ['listOfProperties'])
+    // }
+}
+</script>
+
+<style>
+    #map {
+        margin: 0 auto;
+        height: 500px;
+        width: 90vw;
+        border-radius: 2px;
+        -webkit-box-shadow: 0px 0px 5px 0px rgba(31,35,38,1);
+        -moz-box-shadow: 0px 0px 5px 0px rgba(31,35,38,1);
+        box-shadow: 0px 0px 5px 0px rgba(31,35,38,1);
+    }
+
+    .map-container {
+        width: 100vw;
+        height: 500px;
+        padding-top: 50px;
+        padding-bottom: 50px;
+
+    }
+
+
+    @media only screen and  (max-width: 576px) {
+        #map {
+            width: 100vw;
+            border-radius: 0px;
+            box-shadow: none;
+        }
+    }
+</style>
